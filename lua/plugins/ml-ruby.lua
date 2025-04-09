cf090e67b4c5e25f5549d7420fc590e6d4fe55a3
M Litherland <mlitherland@overdrive.com>
2025-04-09 09:46:59 -0400

    Banner plus disabled ruby_lsp for the time being

--- lua/plugins/ml-ruby.lua
+++ lua/plugins/ml-ruby.lua
@@ -12,34 +12,34 @@ return {
       end
     end,
   },
-  {
-    "williamboman/mason-lspconfig.nvim",
-    optional = true,
-    opts = function(_, opts)
-      opts.ensure_installed =
-        require("astrocore").list_insert_unique(opts.ensure_installed, { "ruby_lsp" })
-    end,
-  },
-  {
-    "WhoIsSethDaniel/mason-tool-installer.nvim",
-    optional = true,
-    opts = function(_, opts)
-      opts.ensure_installed =
-        require("astrocore").list_insert_unique(opts.ensure_installed, { "ruby_lsp" })
-    end,
-  },
+  -- {
+  --   "williamboman/mason-lspconfig.nvim",
+  --   optional = true,
+  --   opts = function(_, opts)
+  --     opts.ensure_installed =
+  --       require("astrocore").list_insert_unique(opts.ensure_installed, { "ruby_lsp" })
+  --   end,
+  -- },
+  -- {
+  --   "WhoIsSethDaniel/mason-tool-installer.nvim",
+  --   optional = true,
+  --   opts = function(_, opts)
+  --     opts.ensure_installed =
+  --       require("astrocore").list_insert_unique(opts.ensure_installed, { "ruby_lsp" })
+  --   end,
+  -- },
   {
     "mfussenegger/nvim-dap",
     optional = true,
     dependencies = { "suketa/nvim-dap-ruby", config = true },
   },
-  {
-    "stevearc/conform.nvim",
-    optional = true,
-    opts = {
-      formatters_by_ft = {
-        ruby = { "ruby_lsp" },
-      },
-    },
-  },
+  -- {
+  --   "stevearc/conform.nvim",
+  --   optional = true,
+  --   opts = {
+  --     formatters_by_ft = {
+  --       ruby = { "ruby_lsp" },
+  --     },
+  --   },
+  -- },
 }
